825b77be62f895f4cb6f3c0b40c3148c85992e1a
JavierLimZH <javier.lim@gmail.com>
2026-08-21 09:21:59 +0800

    Part B Greet and exit

--- src/main/java/Quackers.java
+++ src/main/java/Quackers.java
@@ -3,9 +3,17 @@
  */
 public class Quackers {
     public static void main(String[] args) {
+        String separator = "_".repeat(60);
         String banner = "+--------------------+\n"
                 + "|      QUACKERS      |\n"
                 + "+--------------------+";
+
+        System.out.println(separator);
         System.out.println(banner);
+        System.out.println("Hello! I'm Quackers.");
+        System.out.println("What can I do for you? Quack!");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon! Quack!");
+        System.out.println(separator);
     }
 }

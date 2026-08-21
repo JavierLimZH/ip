5c0030b7dad25af1a61fe14320ea0f26c1af504d
JavierLimZH <javier.lim@gmail.com>
2026-08-21 09:43:44 +0800

    Add mark command for task completion
    Track each task's completion state alongside its description so the existing in-memory task list can mark a selected task as done without introducing another class. Show task status in the list and confirm successful mark operations.

--- src/main/java/Quackers.java
+++ src/main/java/Quackers.java
@@ -12,6 +12,7 @@ public class Quackers {
                 + "|      QUACKERS      |\n"
                 + "+--------------------+";
         String[] tasks = new String[MAX_TASKS];
+        boolean[] isDone = new boolean[MAX_TASKS];
         int taskCount = 0;
 
         System.out.println(separator);
@@ -31,8 +32,19 @@ public class Quackers {
                     System.out.println(separator);
                     break;
                 } else if (command.equals("list")) {
+                    System.out.println("     Here are the tasks in your list:");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                        String statusIcon = isDone[i] ? "X" : " ";
+                        System.out.println("     " + (i + 1) + ".[" + statusIcon + "] " + tasks[i]);
+                    }
+                } else if (command.matches("mark \\d+")) {
+                    int taskIndex = Integer.parseInt(command.substring(5)) - 1;
+                    if (taskIndex >= 0 && taskIndex < taskCount) {
+                        isDone[taskIndex] = true;
+                        System.out.println("     Nice! I've marked this task as done:");
+                        System.out.println("       [X] " + tasks[taskIndex]);
+                    } else {
+                        System.out.println("     Please enter a task number from the list.");
                     }
                 } else if (taskCount < MAX_TASKS) {
                     tasks[taskCount] = command;

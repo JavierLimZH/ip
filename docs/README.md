4b3614e5197d9e43ff061f0cff1364574d6364f9
JavierLimZH <javier.lim@gmail.com>
2026-08-21 09:20:26 +0800

    Part A rename to Quackers

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Quackers User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Quackers.java
@@ -0,0 +1,11 @@
+/**
+ * Minimal entry point for the Quackers chatbot project.
+ */
+public class Quackers {
+    public static void main(String[] args) {
+        String banner = "+--------------------+\n"
+                + "|      QUACKERS      |\n"
+                + "+--------------------+";
+        System.out.println(banner);
+    }
+}
